bc1f7aeddfacec0fb85eb2c563e09911bd985b1b
Artem Rufanov <arufanov@gmail.com>
2011-05-13 23:40:20 +0400

    slash has been changed to unix-like

--- distrib/_local_install.cmd
+++ distrib/_local_install.cmd
@@ -2,14 +2,16 @@ echo off
 
 Rem: set path 
 SET PATH=
-SET PATH=%PATH%;D:\Binutils\Ruby-186-27\bin
-SET PATH=%PATH%;C:\Binutils\Ruby-186-27\bin
-SET PATH=%PATH%;D:\Binutils\rubygems-1.3.5\bin
-SET PATH=%PATH%;C:\Binutils\rubygems-1.3.5\bin
+SET PATH=%PATH%;D:/Binutils/Ruby192/bin
+SET PATH=%PATH%;C:/Binutils/Ruby192/bin
+SET PATH=%PATH%;D:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;C:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;D:/Binutils/rubygems-1.3.5/bin
+SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
 
 echo on
 Rem: set enviroment and execute ant script
-gem install .\output\rmagick_captcha-0.5.0.gem
+gem install ./output/rmagick_captcha-0.5.0.gem
 rem gem uninstall rmagick_captcha
 rem rem gem list --local
 
--- distrib/_local_test.cmd
+++ distrib/_local_test.cmd
@@ -1,25 +1,27 @@
 echo off
 rem examples of usage
 rem
-rem ruby .\anthill_test.rb
-rem ruby .\anthill_test.rb -n test_news_events
+rem ruby ./anthill_test.rb
+rem ruby ./anthill_test.rb -n test_news_events
 
 
 Rem: set path 
 SET PATH=
-SET PATH=%PATH%;D:\Binutils\Ruby-186-27\bin
-SET PATH=%PATH%;C:\Binutils\Ruby-186-27\bin
-SET PATH=%PATH%;D:\Binutils\rubygems-1.3.5\bin
-SET PATH=%PATH%;C:\Binutils\rubygems-1.3.5\bin
+SET PATH=%PATH%;D:/Binutils/Ruby192/bin
+SET PATH=%PATH%;C:/Binutils/Ruby192/bin
+SET PATH=%PATH%;D:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;C:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;D:/Binutils/rubygems-1.3.5/bin
+SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
 
 Rem: set enviroment and execute ant script
-cd .\..\test
-ruby .\anthill_test.rb
-ruby .\binary_decoder_test.rb
-ruby .\binary_encoder_test.rb
-ruby .\byte_buffer_test.rb
-ruby .\decoder_base_test.rb
-ruby .\encoder_base_test.rb
-ruby .\outputter_base_test.rb
-cd .\..\distrib
+cd ./../test
+ruby ./anthill_test.rb
+ruby ./binary_decoder_test.rb
+ruby ./binary_encoder_test.rb
+ruby ./byte_buffer_test.rb
+ruby ./decoder_base_test.rb
+ruby ./encoder_base_test.rb
+ruby ./outputter_base_test.rb
+cd ./../distrib
 
